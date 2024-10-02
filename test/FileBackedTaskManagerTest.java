2cf107293478f3a5048848ce675ee0ec7e9a22d0
DmitriiFomin <dmi7f0min@yandex.ru>
2024-10-02 18:19:12 +0300

    Изменить создание файлов на использование TempFile'ов

--- test/FileBackedTaskManagerTest.java
+++ test/FileBackedTaskManagerTest.java
@@ -7,22 +7,18 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileBackedTaskManagerTest {
-    private static final String FILE_PATH = "C:/Users/4Java/IdeaProjects/java-kanban/test/test_data.cvs";
-    private static final String FILE_PATH1 = "C:/Users/4Java/IdeaProjects/java-kanban/test/test_data1.cvs";
-    private static final String FILE_PATH2 = "C:/Users/4Java/IdeaProjects/java-kanban/test/test_data2.cvs";
+    private File tempFile;
     private FileBackedTaskManager manager;
-    private FileBackedTaskManager manager1;
-    private FileBackedTaskManager manager2;
+
 
     @BeforeEach
-    void setUp() {
-        File file = new File(FILE_PATH);
-        File file1 = new File(FILE_PATH1);
-        File file2 = new File(FILE_PATH2);
-
-        manager = new FileBackedTaskManager(file);
-        manager1 = new FileBackedTaskManager(file1);
-        manager2 = new FileBackedTaskManager(file2);
+    void setUp() throws IOException {
+        tempFile = File.createTempFile("test_data", ".cvs");
+        manager = new FileBackedTaskManager(tempFile);
+    }
+    @AfterEach
+    void tearDown() {
+        tempFile.delete();
     }
 
 
@@ -34,7 +30,7 @@ class FileBackedTaskManagerTest {
         manager.createEpic("new", "New");
         manager.createSubtask("1", "1", 3);
         manager.save();
-        FileBackedTaskManager loadedManager = FileBackedTaskManager.loadFromFile(new File(FILE_PATH));
+        FileBackedTaskManager loadedManager = FileBackedTaskManager.loadFromFile(tempFile);
 
 
         assertEquals(4, loadedManager.getTasks().size());
@@ -44,13 +40,11 @@ class FileBackedTaskManagerTest {
 
     @Test
     void testLoadMultipleTasks() {
-        manager2.createTask("Task 3", "Description 1");
-        manager2.createTask("Task 4", "Description 2");
-        manager2.save();
+        manager.createTask("Task 3", "Description 1");
+        manager.createTask("Task 4", "Description 2");
+        manager.save();
 
-        manager2 = new FileBackedTaskManager(new File(FILE_PATH2));
-
-        FileBackedTaskManager loadedManager = FileBackedTaskManager.loadFromFile(new File(FILE_PATH2));
+        FileBackedTaskManager loadedManager = FileBackedTaskManager.loadFromFile(tempFile);
 
         assertEquals(2, loadedManager.getTasks().size());
         assertEquals("Task 3", loadedManager.getTasks().get(1).getTitle());
@@ -59,8 +53,8 @@ class FileBackedTaskManagerTest {
 
     @Test
     void testSaveAndLoadEmptyFile() {
-        manager1.save();
-        FileBackedTaskManager loadedManager = FileBackedTaskManager.loadFromFile(new File(FILE_PATH1));
+        manager.save();
+        FileBackedTaskManager loadedManager = FileBackedTaskManager.loadFromFile(tempFile);
         assertEquals(0, loadedManager.getTasks().size());
     }
 }
